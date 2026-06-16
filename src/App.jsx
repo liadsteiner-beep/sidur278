1157214054745f843c6688a677a80dfb9cd8cbd5
LiadBannett <liadsteiner@gmail.com>
2026-06-17 00:08:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -473,7 +473,7 @@ export default function App() {
         if (!hasOldNames) setEmployees(local.employees);
       }
       if (local.availability) setAvailability(local.availability);
-      if (local.assigned)     setAssigned(local.assigned);
+      if (local.assigned && false)     setAssigned(local.assigned); // תמיד טען מ-Firebase
       if (local.notes)        setNotes(local.notes);
       if (local.empNotes)     setEmpNotes(local.empNotes);
       if (local.empPasswords) setEmpPasswords(local.empPasswords);
